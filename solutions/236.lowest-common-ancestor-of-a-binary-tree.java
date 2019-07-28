e731ab13d82adc9e36779810d0987bc149b53b23
qubard <qubard@gmail.com>
2019-07-28 17:47:47 -0400

    updated 236

--- solutions/236.lowest-common-ancestor-of-a-binary-tree.java
+++ solutions/236.lowest-common-ancestor-of-a-binary-tree.java
@@ -9,15 +9,14 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        Queue<TreeNode> queue = new ArrayDeque<TreeNode>(); // for bfs
-        HashMap<TreeNode, TreeNode> parents = new HashMap<TreeNode, TreeNode>(); // parents
-        HashSet<TreeNode> p_parents = new HashSet<TreeNode>();
-        HashSet<TreeNode> q_parents = new HashSet<TreeNode>();
+        Queue<TreeNode> queue = new ArrayDeque<>(); // for bfs
+        Map<TreeNode, TreeNode> parents = new HashMap<>(); // parents
+        Set<TreeNode> q_parents = new HashSet<>();
         
         TreeNode currNode;
         queue.add(root);
         
-        while (queue.peek() != null) {
+        while (queue.peek() != null && (!parents.containsKey(p) || !parents.containsKey(q))) {
             currNode = queue.remove();
             
             if (currNode.left != null) {
@@ -31,33 +30,18 @@ class Solution {
             }
         }
         
-        TreeNode ancestor = parents.get(p);
-        p_parents.add(p);
-        
-        while (ancestor != null) {
-            p_parents.add(ancestor);
-            ancestor = parents.get(ancestor);
-        }
-        
-        ancestor = parents.get(q);
+        TreeNode ancestor = parents.get(q);
         q_parents.add(q);
         while (ancestor != null) {
             q_parents.add(ancestor);
             ancestor = parents.get(ancestor);
         }
         
-        // The set with the larger amount of ancestors is the one we should look at
-        Set<TreeNode> chosen = q_parents.size() > p_parents.size() ? p_parents : q_parents;
-        TreeNode chosenNode = q_parents.size() > p_parents.size() ? q : p;
-        
-        ancestor = parents.get(chosenNode);
-        while (ancestor != null) {
-            if (chosen.contains(ancestor)) {
-                return ancestor;
-            }
+        ancestor = p;
+        while (!q_parents.contains(ancestor)) {
             ancestor = parents.get(ancestor);
         }
         
-        return root;
+        return ancestor;
     }
 }
