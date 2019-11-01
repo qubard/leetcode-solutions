2bace1a2ce4140b48071d41972d90b289764ff21
qubard <qubard@gmail.com>
2019-11-01 01:22:52 -0400

    fix typo

--- solutions/072.edit-distance.java
+++ solutions/072.edit-distance.java
@@ -8,7 +8,7 @@ class Solution {
 
         for (int i = 0; i <= word1.length(); i++) {
             dp[i][0] = i;
-        }q
+        }
         
         for (int j = 0; j <= word2.length(); j++) {
             dp[0][j] = j;
